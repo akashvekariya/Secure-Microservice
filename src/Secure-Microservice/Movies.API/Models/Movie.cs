d38096c4b6dbab868b7e7c25ea41b2e2115030e5
Akash Vekariya <akash.vekariya@cm.com>
2023-04-18 19:56:56 +0530

    Add `Movies.API`

--- src/Secure-Microservice/Movies.API/Models/Movie.cs
+++ src/Secure-Microservice/Movies.API/Models/Movie.cs
@@ -2,14 +2,11 @@ namespace Movies.API.Models;
 
 public class Movie
 {
-    // It means that the Title and Genre properties cannot be null, and the
-    // compiler will not generate any warnings or errors if these properties are
-    // accessed without being checked for null. In other words, it tells the
-    // compiler to assume that the properties always have a value and to skip null checks.
-
     public int Id { get; set; }
     public string Title { get; set; } = null!;
     public string Genre { get; set; } = null!;
+    public string Rating { get; set; } = null!;
     public DateTime ReleaseDate { get; set; }
+    public string ImageUrl { get; set; } = null!;
     public string Owner { get; set; } = null!;
 }
--- src/Secure-Microservice/Movies.API/Program.cs
+++ src/Secure-Microservice/Movies.API/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+using Movies.API.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseInMemoryDatabase("MoviesDb"));
+
 var app = builder.Build();
+
+// =========== Seed Database ============
+
+SeedDatabase(app);
+
+static void SeedDatabase(IHost host)
+{
+    using var scope = host.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var moviesContext = services.GetRequiredService<MoviesContext>();
+    MoviesContextSeed.SeedData(moviesContext);
+}
+
+// ======================================
 
 if (app.Environment.IsDevelopment())
 {
