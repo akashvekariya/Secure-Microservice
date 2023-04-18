215232ff7ca4488f6c33873ff96582bd4e320d1b
Akash Vekariya <akashvekariya@yahoo.com>
2023-04-19 00:01:31 +0530

    Update IdentityServer

--- src/Secure-Microservice/IdentityServer/Configurations/Config.cs
+++ src/Secure-Microservice/IdentityServer/Configurations/Config.cs
@@ -5,9 +5,20 @@ namespace IdentityServer.Configurations;
 
 public class Config
 {
-    public static IEnumerable<Client> Clients => Array.Empty<Client>();
+    public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope> { new("movieAPI", "Movie API") };
+
+    public static IEnumerable<Client> Clients => new List<Client>
+    {
+        new()
+        {
+            ClientId = "MovieClient",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedScopes = { "movieAPI" }
+        }
+    };
+
     public static IEnumerable<ApiResource> ApiResources => Array.Empty<ApiResource>();
-    public static IEnumerable<ApiScope> ApiScopes => Array.Empty<ApiScope>();
     public static IEnumerable<IdentityResource> IdentityResources => Array.Empty<IdentityResource>();
     public static List<TestUser> TestUsers => new();
 }
