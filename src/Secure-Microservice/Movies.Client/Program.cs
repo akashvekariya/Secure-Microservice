378fe5725ea38e5e07effa0a33ff63c83e3d7ef4
Akash Vekariya <akashvekariya@yahoo.com>
2023-04-22 20:32:33 +0530

    Add `Movies.Client` and Scafolding for MoviesController

---
+++ src/Secure-Microservice/Movies.Client/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.Client.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MoviesContext") ??
+        throw new InvalidOperationException("Connection string 'MoviesContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
